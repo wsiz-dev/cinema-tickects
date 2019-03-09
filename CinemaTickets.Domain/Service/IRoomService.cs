2dd3a9d50f0594a93367dd5fb861b09a61a5280c
Dawid Pereira <pereiradawid@outlook.com>
2019-03-09 23:04:19 +0100

    Refactor RegisterSeanceCommand Configure Dependency Injection

---
+++ CinemaTickets.Domain/Service/IRoomService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CinemaTickets.Domain.Entities;
+
+namespace CinemaTickets.Domain.Service
+{
+    public interface  IRoomService
+    {
+        double GetTimeSpanBeforeSeanceDate(Room room, DateTime seanceDate, int seanceTime);
+        double GetTimeSpanAfterSeanceDate(Room room, DateTime seanceDate, int seanceTime);
+    }
+}
